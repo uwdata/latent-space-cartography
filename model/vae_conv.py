748a28b8a4d75f4cda13ebd0ca33fbd3a823e829
Yang Liu <yliu0@cs.washington.edu>
2018-03-03 12:59:23 -0800

    = log, learning rate, image snapshot (but this causes a weird val_loss inf bug)

--- model/vae_conv.py
+++ model/vae_conv.py
@@ -4,23 +4,27 @@ from PIL import Image
 import numpy as np
 from scipy.stats import norm
 import json
+import os
 
 from keras.layers import Input, Dense, Lambda, Flatten, Reshape
 from keras.layers import Conv2D, Conv2DTranspose
 from keras.models import Model, model_from_json
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
+from keras.callbacks import Callback, ReduceLROnPlateau
 from keras import backend as K
 from keras import metrics
 
 latent_dim = 64
 
+base = '/home/yliu0/data/'
 # input path
-inpath = '/home/yliu0/data/logos.hdf5'
+inpath = base + 'logos.hdf5'
 # output path
-fpath = '/home/yliu0/data/logo_result/'
-# model path
-mpath = 'logo_model_dim={}.json'.format(latent_dim)
-wpath = 'logo_model_dim={}.h5'.format(latent_dim)
+outbase = base + '/logo_result/{}/'.format(latent_dim)
+# saved model and weights
+mpath = outbase + 'logo_model_dim={}.json'.format(latent_dim)
+wpath = outbase + 'logo_model_dim={}.h5'.format(latent_dim)
+logpath = outbase + 'logo_log_dim={}.csv'.format(latent_dim)
 
 # input image dimensions
 img_rows, img_cols, img_chns = 64, 64, 3
@@ -186,6 +190,9 @@ def recover_generator (vae):
     _x_decoded_mean_squash = decoder_mean_squash(_x_decoded_relu)
     return Model(decoder_input, _x_decoded_mean_squash)
 
+'''
+ Load or instantiate the VAE, encoder and decoder mdoels
+'''
 def init_model (fn, weights):
     try:
         with open(fn, 'r') as infile:
@@ -214,62 +221,101 @@ def init_model (fn, weights):
 
     return vae, encoder, decoder
 
+'''
+ Load training data.
+'''
 def load_data (fpath):
     f = h5py.File(fpath, 'r')
     dset = f['logos']
 
-    x_train = dset[:10000]
-    x_test = dset[10000:12000]
+    x_train = dset[:15000]
+    x_test = dset[15000:]
 
     return x_train, x_test
 
-# train the VAE on our logo dataset
-x_train, x_test = load_data(inpath)
+'''
+ Custom callback to hook into model.fit
+'''
+class Visualizer(Callback):
+    def __init__(self, x_test, encoder, decoder):
+        self.x_test = x_test
+        self.encoder = encoder
+        self.decoder = decoder
 
-x_train = x_train.astype('float32') / 255.
-x_train = x_train.reshape((x_train.shape[0],) + original_img_size)
-x_test = x_test.astype('float32') / 255.
-x_test = x_test.reshape((x_test.shape[0],) + original_img_size)
+    def on_train_end(self, logs={}):
+        visualize(self.x_test, self.encoder, self.decoder, 'end')
 
-# initialize our VAE model
-vae, encoder, generator = init_model(mpath, wpath)
-vae.summary()
+    def on_epoch_end(self, epoch, logs={}):
+        return # turn off
+        # every 10 epoch
+        if epoch % 10 == 9:
+            visualize(self.x_test, self.encoder, self.decoder, epoch + 1)
 
-print('x_train.shape:', x_train.shape)
+'''
+ Entry point: load data, create model and start training
+'''
+def train ():
+    # train the VAE on our logo dataset
+    x_train, x_test = load_data(inpath)
 
-vae.fit(x_train,
-        shuffle=True,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(x_test, None),
-        callbacks=[
-            ModelCheckpoint(wpath, save_best_only=True, save_weights_only=True),
-            EarlyStopping(monitor='val_loss', patience=5, verbose=0)])
+    x_train = x_train.astype('float32') / 255.
+    x_train = x_train.reshape((x_train.shape[0],) + original_img_size)
+    x_test = x_test.astype('float32') / 255.
+    x_test = x_test.reshape((x_test.shape[0],) + original_img_size)
 
-# encode and decode
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-x_test_decoded = generator.predict(x_test_encoded)
+    # initialize our VAE model
+    vae, encoder, generator = init_model(mpath, wpath)
+    vae.summary()
 
-m = 5
-original = np.zeros((img_rows * m, img_cols * m, img_chns), 'uint8')
-reconstructed = np.zeros((img_rows * m, img_cols * m, img_chns), 'uint8')
+    print('x_train.shape:', x_train.shape)
 
-def to_image (array):
-    array = array.reshape(img_rows, img_cols, img_chns)
-    array *= 255
-    return array.astype('uint8')
+    cp = ModelCheckpoint(wpath, save_best_only=True, save_weights_only=True)
+    stop = EarlyStopping(monitor='val_loss', patience=20, verbose=0)
+    csv_logger = CSVLogger(logpath, append = True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=5, min_lr=0.001)
+    vis = Visualizer(x_test, encoder, generator)
+    vae.fit(x_train,
+            shuffle=True,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_test, None),
+            callbacks=[cp, stop, csv_logger, vis])
 
-for i in range(m):
-    for j in range(m):
-        k = i * m + j
-        orig = to_image(x_test[k])
-        re = to_image(x_test_decoded[k])
-        original[i * img_rows: (i + 1) * img_rows,
-               j * img_cols: (j + 1) * img_cols] = orig
-        reconstructed[i * img_rows: (i + 1) * img_rows,
-               j * img_cols: (j + 1) * img_cols] = re
+'''
+ Create images of original samples versus reconstructed.
+'''
+def visualize (x_test, encoder, generator, suffix=''):
+    # encode and decode
+    x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+    x_test_decoded = generator.predict(x_test_encoded)
 
-img = Image.fromarray(original, 'RGB')
-img.save(fpath + 'original.png')
-img = Image.fromarray(reconstructed, 'RGB')
-img.save(fpath + 'reconstructed.png')
+    m = 5
+    original = np.zeros((img_rows * m, img_cols * m, img_chns), 'uint8')
+    reconstructed = np.zeros((img_rows * m, img_cols * m, img_chns), 'uint8')
+
+    def to_image (array):
+        array = array.reshape(img_rows, img_cols, img_chns)
+        array *= 255
+        return array.astype('uint8')
+
+    for i in range(m):
+        for j in range(m):
+            k = i * m + j
+            orig = to_image(x_test[k])
+            re = to_image(x_test_decoded[k])
+            original[i * img_rows: (i + 1) * img_rows,
+                j * img_cols: (j + 1) * img_cols] = orig
+            reconstructed[i * img_rows: (i + 1) * img_rows,
+                j * img_cols: (j + 1) * img_cols] = re
+
+    img = Image.fromarray(original, 'RGB')
+    img.save('{}original.png'.format(outbase))
+    img = Image.fromarray(reconstructed, 'RGB')
+    img.save('{}reconstructed_{}.png'.format(outbase, suffix))
+
+if __name__ == '__main__':
+    if not os.path.exists(outbase):
+        os.makedirs(outbase)
+
+    train()

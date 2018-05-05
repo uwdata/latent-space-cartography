47e7445440b127743508494ab011d345cf0f7185
Yang Liu <yliu0@cs.washington.edu>
2018-05-05 15:55:30 -0700

    = refactor detail card to be a separate component

---
+++ client/src/layouts/DetailCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card mb-3" v-if="detail_point">
+    <div class="card-header">Details</div>
+    <div class="card-body">
+      <p>{{detail_point.name}}</p>
+      <div class="d-flex" style="font-size: 0.8em;">
+        <div class="p1">
+          <img :src="imageUrl(detail_point)" />
+        </div>
+        <div class="w-100 ml-2">
+          <div class="mb-2">
+            <b>Industry: </b>
+            {{detail_point.industry}}
+          </div>
+          <div class="mb-2">
+            <b>Data Source: </b>
+            {{detail_point.source}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {store} from '../controllers/config'
+
+  export default {
+    name: 'DetailCard',
+    props: {
+      detail_point: {
+        required: true
+      }
+    },
+    methods: {
+      // helper
+      imageUrl (p) {
+        return store.getImageUrl(p.i)
+      }
+    }
+  }
+</script>
--- client/src/pages/AnalogyPage.vue
+++ client/src/pages/AnalogyPage.vue
@@ -26,27 +26,8 @@
           <div v-if="brushed.length" class="col-4 bd-left">
 
             <!--Details of a Logo-->
-            <div class="card mb-3" v-if="detail_point">
-              <div class="card-header">Details</div>
-              <div class="card-body">
-                <p>{{detail_point.name}}</p>
-                <div class="d-flex" style="font-size: 0.8em;">
-                  <div class="p1">
-                    <img :src="imageUrl(detail_point)" />
-                  </div>
-                  <div class="w-100 ml-2">
-                    <div class="mb-2">
-                      <b>Industry: </b>
-                      {{detail_point.industry}}
-                    </div>
-                    <div class="mb-2">
-                      <b>Data Source: </b>
-                      {{detail_point.source}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <detail-card :detail_point="detail_point"
+                         v-if="detail_point"></detail-card>
 
             <!--List of Brushed Points-->
             <div class="card mb-3" v-if="brushed.length">
@@ -126,6 +107,7 @@
   import ChartButtons from '../layouts/ChartButtons.vue'
   import HelpButton from '../layouts/HelpButton.vue'
   import InterpolatePanel from '../layouts/InterpolatePanel.vue'
+  import DetailCard from '../layouts/DetailCard.vue'
 
   import Scatter from '../controllers/scatter_analogy'
   import {store, log_debug, TRAIN_SPLIT} from '../controllers/config'
@@ -208,6 +190,7 @@
       ChartButtons,
       HelpButton,
       InterpolatePanel,
+      DetailCard,
       VueLoading
     },
     name: 'AnalogyPage',

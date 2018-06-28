d4caf5a9d715d901fee2120069a1844e24c35d83
Yang Liu <yliu0@cs.washington.edu>
2018-06-27 21:30:15 -0700

    = tybalt: manually writing right outer join ...

--- analysis/tybalt_util.py
+++ analysis/tybalt_util.py
@@ -17,14 +17,16 @@ class Util(object):
         self.p_header = os.path.join(base, 'data', 'pancan_scaled_zeroone_rnaseq_header.csv')
         self.p_id = os.path.join(base, 'data', 'patient_id.csv')
         self.p_meta = os.path.join(base, 'data', 'ov_subtype_info.tsv')
+        # schema index in the meta table
+        self.i_subtype = 2
 
-    # read tsv, discarding the first row and first column
-    def read_tsv (self, fn, dtype=float):
+    # read tsv, discarding the first row and (optionally) the first column
+    def read_tsv (self, fn, dtype=float, col_start=1):
         res = []
         with open(fn, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
             for row in reader:
-                res.append(row[1:])
+                res.append(row[col_start:])
         res = np.asarray(res[1:], dtype=dtype)
         return res
 
@@ -60,4 +62,28 @@ class Util(object):
 
     # read the meta data (see hgsc_subtypes_tybalt.ipynb for schema)
     def read_meta (self):
-        return self.read_tsv(self.p_meta, str)
+        return self.read_tsv(self.p_meta, str, 0)
+
+    def right_outer_join (self, left, right):
+        # convert left to a dictionary
+        lookup = {}
+        for i in range(left.shape[0]):
+            lookup[left[i]] = i
+
+        # right outer join
+        ids = []
+        for i in range(len(right)):
+            if right[i] in lookup:
+                ids.append([i, lookup[right[i]]])
+
+        # each row is [index in right, index in left]
+        return np.asarray(ids, dtype=int)
+
+    # get the indices of right outer join of meta with patient ID
+    def join_meta (self):
+        pid = self.read_id()
+        meta = self.read_meta()[:, 0]
+        ids = self.right_outer_join(meta, pid)
+
+        # shape: (295, 2)
+        return ids

f839f6c21c34a7e37a6c332ca21fc909a2ba3e97
Yang Liu <yliu0@cs.washington.edu>
2018-09-02 15:06:02 -0700

    Word2vec config

---
+++ model/config_word2vec.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# configurations of Word2Vec word embedding on Wikipedia data
+
+dset = 'word2vec'
+data_type = 'text'
+dims = [300]
+metric = 'cosine'
+
+# MySQL table schema
+schema_meta = 'i, name'
+schema_header = None

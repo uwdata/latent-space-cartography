a2369cd60937dee74d949bb9554d955c73ad8b6b
Yang Liu <yliu0@cs.washington.edu>
2018-09-02 23:37:39 -0700

    Run analogy test set on GloVe

--- util/word_embedding/benchmark.py
+++ util/word_embedding/benchmark.py
@@ -4,6 +4,27 @@ import os
 import sys
 import logging
 from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+
+base = '/Users/yliu0/data/word/'
+dims = [50, 100, 200, 300]
+
+def analogy_w2v ():
+    fn = './dim300.bin.gz'
+    wv = KeyedVectors.load_word2vec_format(fn, binary=True)
+    analogy_scores = wv.accuracy('./questions-words.txt')
+
+def convert_glove ():
+    for dim in dims:
+        fn = base + 'raw/glove.6B/glove.6B.{}d.txt'.format(dim)
+        pout = base + 'raw/glove_6b_w2v/glove.6B.{}d.w2v.txt'.format(dim)
+        glove2word2vec(fn, pout)
+
+def analogy_glove ():
+    for dim in dims:
+        fn = base + 'raw/glove_6b_w2v/glove.6B.{}d.w2v.txt'.format(dim)
+        wv = KeyedVectors.load_word2vec_format(fn)
+        analogy_scores = wv.accuracy(base + 'benchmarks/questions-words.txt')
 
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
@@ -11,6 +32,6 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
     logging.root.setLevel(level=logging.INFO)
 
-    fn = './dim300.bin.gz'
-    wv = KeyedVectors.load_word2vec_format(fn, binary=True)
-    analogy_scores = wv.accuracy('./questions-words.txt')
+    # analogy_w2v()
+    # convert_glove()
+    analogy_glove()
